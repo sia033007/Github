81b283f47e87a6b07c0a4a9a50b498a7a32fe636
DESKTOP-8IO3ALF\HeisenBerg <siavashmehmandoost@gmail.com>
2020-12-15 06:52:32 -0800

    Add Plane and Cube

---
+++ Assets/Scripts/NaveSample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NaveSample : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,100))
+            {
+                agent.destination = hit.point;
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+    }
+}
